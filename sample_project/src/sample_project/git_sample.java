8fe23700b732804e7773614bc299c048d5f2917a
nagaraju <nagaraju@kyatiservices>
2019-04-19 08:47:07 +0530

    git_test

---
+++ sample_project/src/sample_project/git_sample.java
@@ -0,0 +1,10 @@
+package sample_project;
+
+public class git_sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My sample git example");
+	}
+
+}
